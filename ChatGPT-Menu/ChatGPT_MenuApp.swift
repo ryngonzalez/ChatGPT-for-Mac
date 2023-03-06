6dd3530357831ca63c888bf22d57f105fce9ab00
Kathryn Gonzalez <kathrynmichellegonzalez@gmail.com>
2023-03-06 14:32:42 -0800

    Initial Commit

---
+++ ChatGPT-Menu/ChatGPT_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_MenuApp.swift
+//  ChatGPT-Menu
+//
+//  Created by Kathryn Gonzalez on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
